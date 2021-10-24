2c150f7230b6c1a908c699170cfe571e1c1d9bfc
Nikita Morunov <niknixad@gmail.com>
2021-10-24 11:06:49 +0400

    Add test for gravity between two bodies

---
+++ SimulationTests/GravityTests.cpp
@@ -0,0 +1,36 @@
+#include "catch.hpp"
+#include "IWorld.hpp"
+#include "Simulation.hpp"
+
+#include "components/Position.hpp"
+#include "components/Mass.hpp"
+#include "components/Acceleration.hpp"
+
+const double gravConst = 6.6743e-11;
+
+TEST_CASE("All bodies with mass must gravitate", "[Gravity]")
+{
+	Simulation simul;
+	IWorld* world = simul.getWorld();
+
+	Position firstPos{ 1,0,0 };
+	Mass firstMass{ 5e10 };
+
+	Position secondPos{ 1,1,0 };
+	Mass secondMass{ 15e10 };
+
+	int firstBody = world->createEntity();
+	world->attach(firstBody, firstMass);
+	world->attach(firstBody, firstPos);
+	auto& firstBodyAcc = world->attach(firstBody, Acceleration{ 0,0,0 });
+
+	int secondBody = world->createEntity();
+	auto& secondBodyPos = world->attach(secondBody, secondPos);
+	auto& secondBodyAcc = world->attach(secondBody, Acceleration{ 0,0,0 });
+	world->attach(secondBody, secondMass);
+
+	simul.gravity();
+
+	REQUIRE(Approx(firstBodyAcc.y) == gravConst * secondMass.value);
+	REQUIRE(Approx(secondBodyAcc.y) == -gravConst * firstMass.value);
+}
